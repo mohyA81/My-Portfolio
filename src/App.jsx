c9b21417f56dc8cabd00f542f9e8430b74e66f23
mohadese <mohadese.atlookhanloo@gmail.com>
2026-08-06 23:21:10 +0330

    Initial React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#030007]">
+      <h1 className="text-5xl font-bold text-purple-300">
+        Tiara Portfolio
+      </h1>
+    </div>
+  );
+}
+
+export default App;
